d5513bd1ec3792d1561d98914aece8c1461a2911
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-03-04 09:25:55 +0530

    Backend CA2 Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### The Deyplotment Link - 
+
---
+++ signUp.js
@@ -0,0 +1,19 @@
+app.post('/username',(req,res) => {
+
+})
+
+
+app.post('/email',(req,res) => {
+
+})
+
+
+app.post('/password',(req,res) => {
+
+})
+
+
+app.post('/dob',(req,res) => {
+
+})
+
