04f24d964341e3d6f6fa39f5efe94b2f82ebe6d4
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-03-04 10:04:59 +0530

    Backend CA2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### The Deyplotment Link - 
 
+
+
--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const app = express();
 app.use(express.json);
 const port = 3000;
 
+// module.export(signUp.js);
+
+app.get('/user',(req,res) => {
+    console.log("This is a get request");
+    
+})
+
+
+app.listen(port,(req,res) => {
+    console.log(`You are listening to port http://localhost:${port}`);
+    
+})
+
